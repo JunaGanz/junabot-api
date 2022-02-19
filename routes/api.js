110b1a9e5be57ecf10ad6aed561f2bd9af64eb1d
JunaGanz <93413322+JunaGanz@users.noreply.github.com>
2022-02-19 08:14:44 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -22,8 +22,8 @@ var lolkill = db.get("lolkill");
 	console.log('')
 }
 
-var creator = 'ZeeoneOfc' // ubah jadi nama lu
-const listkey = ["Alphabot","Zeeone","ZeeoneOfc"]; // ubah apikey nya, tambah aja klo mau
+var creator = 'JunaAtreides' // ubah jadi nama lu
+const listkey = ["Atreides","Juna","JunaAtreides"]; // ubah apikey nya, tambah aja klo mau
 
 var axios = require('axios');
 var qs = require('qs');
